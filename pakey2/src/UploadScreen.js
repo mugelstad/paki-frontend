71d0ae60f0558b69028166e3056a2fa41b08ab3b
Yilin Liu <yilin_liu8159@hotmail.com>
2018-08-07 11:24:36 -0700

    frontend upload multipke ;

--- pakey2/src/UploadScreen.js
+++ pakey2/src/UploadScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, Button, Image } from 'react-native';
-import { Camera, Permissions, ImagePicker} from 'expo';
+import { Text, View, TouchableOpacity, Button, Image, ScrollView } from 'react-native';
+import { Camera, Permissions, ImagePicker } from 'expo';
 
 export default class UploadScreen extends React.Component {
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
-    image: null
+    images: ["https://searchenginewatch.com/wp-content/uploads/sites/25/2016/08/thinking-emoji.png",
+      "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-11/256/unamused-face.png"
+            ]
   };
 
   async componentWillMount() {
@@ -16,30 +18,34 @@ export default class UploadScreen extends React.Component {
     this.setState({ hasCameraPermission: status === 'granted' });
   }
 
-  _pickImage = async () => {
+  pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       aspect: [4, 3],
     });
-
-    console.log(result);
-
     if (!result.cancelled) {
-      this.setState({ image: result.uri });
+      var newArr = this.state.images.slice();
+      newArr.push(result.uri)
+      this.setState({ images: newArr });
     }
   };
 
   postPicture() {
     const apiUrl = 'https://ee4f8815.ngrok.io/upload';
-    const uri = this.state.image;
-    const uriParts = uri.split('.');
-    const fileType = uriParts[uriParts.length - 1];
     const formData = new FormData();
-      formData.append('photo', {
+
+    const formArr = this.state.images.map(photo => {
+      const uri = photo;
+      const uriParts = uri.split('.');
+      const fileType = uriParts[uriParts.length - 1];
+      return ({
         uri,
         name: `photo.${fileType}`,
         type: `image/${fileType}`,
-      });
+      })
+    })
+    formData.append('photos', formArr);
+
     const options = {
       method: 'POST',
       body: formData,
@@ -51,21 +57,48 @@ export default class UploadScreen extends React.Component {
     return fetch(apiUrl, options);
   }
 
+  // postPicture() {
+  //   const apiUrl = 'https://ee4f8815.ngrok.io/upload';
+  //   const uri = this.state.image;
+  //   const uriParts = uri.split('.');
+  //   const fileType = uriParts[uriParts.length - 1];
+  //   const formData = new FormData();
+  //     formData.append('photo', {
+  //       uri,
+  //       name: `photo.${fileType}`,
+  //       type: `image/${fileType}`,
+  //     });
+  //   const options = {
+  //     method: 'POST',
+  //     body: formData,
+  //       headers: {
+  //         Accept: 'application/json',
+  //         'Content-Type': 'multipart/form-data',
+  //       },
+  //     };
+  //   return fetch(apiUrl, options);
+  // }
+
   render() {
-  let { image } = this.state;
+  let { images } = this.state;
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <ScrollView>
       <Button
         title="Pick an image from camera roll"
-        onPress={this._pickImage}
+        onPress={this.pickImage}
       />
-      {image &&
-        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      {images &&
+        images.map(photo => {
+          return ( <Image source={{ uri: photo }} style={{ width: 200, height: 200 }} />)
+         })
+       }
       <Button
-        title="upload to server"
+        title="Save"
         onPress={() => this.postPicture()}
       />
+      </ScrollView>
     </View>
   );
 }
